d960e42e2e6a763e0cc862363060e319fa29e78a
salikur6 <salikur6@gmail.com>
2022-04-04 03:52:35 +0600

    changes in card . create links and button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useReviews from '../../Hooks/useReviews';
 import Review from '../Review/Review';
 import './Home.css'
@@ -13,8 +14,8 @@ const Home = () => {
             <div className='row my-5 align-items-center home-content'>
                 <div className="col-lg-7 col-md-7">
                     <h1 className='fw-bold'>Your next Laptop</h1>
-                    <h1 className='fw-bold text-primary'>Your best Laptop</h1>
-                    <p className=''>Laptops combine all the input/output components and capabilities of a desktop computer, including the display screen, small speakers, a keyboard, data storage device.</p>
+                    <h1 className='fw-bold text-primary mb-4'>Your best Laptop</h1>
+                    <p className='mb-4'>Laptops combine all the input/output components and capabilities of a desktop computer, including the display screen, small speakers, a keyboard, data storage device.</p>
                     <button className='fw-bold btn btn-primary live-btn'>Live Demo</button>
                 </div>
                 <div className="col-lg-5 col-md-5">
@@ -22,18 +23,18 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className='customer-reviews row g-3'>
+            <div className='customer-reviews row g-3 justify-content-center'>
 
-                <h2>Review: {revSlice.length}</h2>
+                <h2 className='text-center fw-bold'>Customer Reviews: ({revSlice.length})</h2>
 
                 {
-
                     reviews.slice(0, 3).map(review => <Review key={review.id} review={review}></Review>)
-
-
                 }
 
 
+            </div>
+            <div className='d-flex justify-content-center'>
+                <Link to='/reviews' className='my-4 btn btn-primary px-5 fw-bold'>See All Reviews</Link>
             </div>
         </div>
     );
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Review = ({ review }) => {
     const { name, post, rating } = review;
     return (
-        <div className="gap-3 m-3 col-lg-3 col-md-5 border">
-            <div className='col-md-12'>
-                <h3>Name: {name}</h3>
+        <div className="gap-3 m-3  my-5 col-lg-3 col-md-5 card border-2 border-warning bg-dark text-white">
+            <div className='col-md-12 py-3 px-2'>
+                <h3 className='fw-bold'>Name: {name}</h3>
                 <p>{post}</p>
-                <p><small>Rating: {rating}</small></p>
+                <p className='fw-bold'><small>Rating: {rating}</small></p>
             </div>
         </div>
     );

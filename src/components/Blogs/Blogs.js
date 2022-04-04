2b8d9e1ead79f82134f7112dad377805043560ac
salikur6 <salikur6@gmail.com>
2022-04-04 23:46:20 +0600

    blogs page design

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h2>Blogs</h2>
+            <div>
+                <h2>1. What is context API ?</h2>
+                <p> Context is primarily used when some data needs to be accessible by many components at different nesting levels. Contaxt api is a very powerful and very useful thing. If you only want to avoid passing some props through many levels, component composition is often a simpler solution than context. Context API is the way to create global variables that can be passed around the component tree. Context API is an alternative to passing props manually in a component tree.</p>
+            </div>
+
+
         </div>
     );
 };
